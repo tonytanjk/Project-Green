d50269ae702363cb9cbd5fbea87cc18b8e2e07fc
tony <tantony.24.ichat.sp.edu.sg>
2024-05-08 23:13:31 +0800

    First Commit

---
+++ src/Project.py
@@ -0,0 +1,27 @@
+#hydroponics system 
+from test import print_test
+
+def pH_level(): # Determine pH level of Soil ()
+    print("Write Code Here")
+
+def amb_temp(): # Grab Ambient Temperature and determine if fan need to be activated
+    print("Write Code Here")
+
+def relative_humidity(): # Relative Humidity chk
+    print("Write Code Here")
+
+def amb_light_intensity(): # Surrounding light intensity (controlling UV lights)
+    print("Write Code Here")
+
+def EC_level(): # Determine EC level and see if needed to release more or less nutrient solution
+    print("Write Code Here")
+
+def main_web(): #Dashboard to gather all sensor inputs
+    print("Write Code Here")
+
+def main(): # Main Function
+    amb_temp()
+
+if __name__ == "__main__":
+    main()
+    print_test()
