aaf372c20b2314f673e1d04b1d283f609e6da9a1
tony <tantony.24.ichat.sp.edu.sg>
2024-06-25 14:57:15 +0800

    tony Changes

--- src/Project.py
+++ src/Project.py
@@ -1,14 +1,39 @@
 #hydroponics system 
-from test import print_test
+
+import time
+from threading import Thread
+import queue
+
+from hal import hal_led as led
+from hal import hal_lcd as LCD
+from hal import hal_adc as adc
+from hal import hal_buzzer as buzzer
+from hal import hal_keypad as keypad
+from hal import hal_moisture_sensor as moisture_sensor
+from hal import hal_input_switch as input_switch
+from hal import hal_ir_sensor as ir_sensor
+from hal import hal_rfid_reader as rfid_reader
+from hal import hal_servo as servo
+from hal import hal_temp_humidity_sensor as temp_humid_sensor
+from hal import hal_usonic as usonic
+from hal import hal_dc_motor as dc_motor
+from hal import hal_accelerometer as accel
+lcd = LCD.lcd()
+lcd.lcd_clear()
+
+def main():
+    ...
 
 def pH_level(): # Determine pH level of Soil ()
     print("Write Code Here")
 
-def amb_temp(): # Grab Ambient Temperature and determine if fan need to be activated
-    print("Write Code Here")
+def amb_temp_humi(): # Grab Ambient Temperature and humidity to determine if fan need to be activated
+    temperature, humidity = temp_humid_sensor.read_temp_humidity()
+    lcd.lcd_display_string("Temperature "  +str(temperature), 1)  
+    lcd.lcd_display_string("Humidity "  +str(humidity), 2)
+    time.sleep(2)
+    return temperature, humidity
 
-def relative_humidity(): # Relative Humidity chk
-    print("Write Code Here")
 
 def amb_light_intensity(): # Surrounding light intensity (controlling UV lights)
     print("Write Code Here")
@@ -24,4 +49,3 @@ def main(): # Main Function
 
 if __name__ == "__main__":
     main()
-    print_test()
---
+++ src/test_main.py
@@ -0,0 +1,5 @@
+#test_main.py
+import Project
+
+def test_temp_humidity():
+    Project.amb_temp()
